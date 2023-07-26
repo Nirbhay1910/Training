219937d3e41d2f0348dfa2f7544abf5a7af25a86
Nirbhay Jain <nirbhay@Nirbhays-MacBook-Air.local>
2023-07-26 13:50:18 +0530

    Day 2 - learnt about functions, window and this keyword, undefined and scoping

--- js and jquery/namaste javascript/index.js
+++ js and jquery/namaste javascript/index.js
@@ -1,4 +1,4 @@
-/// hoisting 
+///////// hoisting /////////
 
 getName(); //outputs Nirbhay Jain
 console.log(x); // undefined
@@ -22,3 +22,59 @@ var getName2 = () => {
 var getName3 = function () {
   console.log('Yayy');
 } // It again behaves as a variable
+
+
+
+/////// FUNCTIONS //////
+
+
+var x=1;
+a();
+b();
+console.log(x);
+
+function a(){
+  var x=10;
+  console.log(x);
+}
+function b(){
+  var x=20;
+  console.log(x);
+}
+
+
+
+//////// window and this keyword ///////
+
+console.log(window);
+console.log(this); //at global level this points to window
+// window.alert("yo");
+console.log(window.x);
+console.log(this.x);
+
+
+////// undefined and vars ////////
+
+console.log(nj);
+if(nj===undefined){
+  console.log("nj is undefined");
+} else console.log("nj is defined");
+var nj=70;
+console.log(nj);
+nj = "NIrbhayJain";
+console.log(nj);
+
+
+////// Scope and Lexical Environment //////
+
+function scoping(){
+  var girl = 5;
+  scoping2();
+  function scoping2(){
+    console.log(hp);//hp doesnt exist in local so it go to the lexical env of its parent to find hp and goes on till it finds 'hp' or null;
+  }//scoping2 is lexically inside scoping fn i.e it contains lexical env of its parent
+}//scoping is lexically inside global i.e scoping has lexical env of global
+//global's lexical env points to null
+var hp = 10;
+scoping();
+// console.log(girl); //Error
