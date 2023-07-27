0e8436ddd7a1ad0f1b7507e9c502eed80a68b7a6
Nirbhay Jain <nirbhay@Nirbhays-MacBook-Air.local>
2023-07-27 19:27:51 +0530

    learnt about Ajax

---
+++ AJAX/ajax.js
@@ -0,0 +1,117 @@
+let fetchBtn = document.getElementById("fetchBtn");
+let backupBtn = document.getElementById("backupBtn");
+let popBtn = document.getElementById("popBtn");
+
+fetchBtn.addEventListener("click", buttonClickHandler);
+backupBtn.addEventListener("click", postButtonHandler);
+popBtn.addEventListener("click", popButtonHandler);
+
+function buttonClickHandler() {
+  console.log("You clicked the button");
+  //instantiate an xhr object
+  const xhr = new XMLHttpRequest();
+
+  //open the object
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true); //true means async
+
+  // what to do on progress (optional)
+  xhr.onprogress = function () {
+    console.log("On progress");
+  };
+
+  //if we have to do something on state change (optional)
+  xhr.onreadystatechange = function () {
+    console.log("ready state is ", xhr.readyState);
+    /*
+     0 -> unsent i.e open method is not yet initialized
+     1 -> opened i.e request is initialized
+     2 -> headers are received as send() is invoked
+     3 -> request is still in process
+     4 -> completion of request
+    */
+  };
+
+  // what to do when response is ready
+  xhr.onload = function () {
+    if (this.status === 200) {
+      console.log(this.responseText);
+      console.log(this);
+    } else {
+      console.log("some error");
+    }
+    // 200 - OK, 201 - created
+    // 400- bad request, 401- unauthorized, 403 - forbidden, 404- not found
+    //502 - bad gateway, 504 - gateway timeout
+  };
+
+  //send the request
+  xhr.send();
+}
+
+function postButtonHandler() {
+  console.log("You clicked the button");
+  //instantiate an xhr object
+  const xhr = new XMLHttpRequest();
+
+  //open the object
+  xhr.open("POST", "https://dummyjson.com/products/add", true); //true means async
+
+  //Tells content type of data sending in POST request
+  // xhr.getResponseHeader("content-type", "application/x-www-form-urlencoded"); // for url encoded
+  xhr.getResponseHeader("content-type", "application/json"); //for json type
+
+  // what to do on progress (optional)
+  xhr.onprogress = function () {
+    console.log("On progress");
+  };
+
+  //if we have to do something on state change (optional)
+  xhr.onreadystatechange = function () {
+    console.log("ready state is ", xhr.readyState);
+  };
+
+  // what to do when response is ready
+  xhr.onload = function () {
+    if (this.status === 200) {
+      console.log(this.responseText);
+      console.log(this);
+    } else {
+      console.log("some error");
+    }
+  };
+
+  params = "title=BMW&owner=Pencil&age=23"; //if we are using url encoded
+  //send the request
+  xhr.send(params);
+}
+
+function popButtonHandler() {
+  console.log("Pop button is clicked");
+  const xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    "https://api.slingacademy.com/v1/sample-data/files/employees.json",
+    true
+  );
+  xhr.onload = function () {
+    if (this.status === 200) {
+      let obj = JSON.parse(this.responseText);
+      console.log(obj.employees);
+      let list = document.getElementById("list");
+      let str = "";
+      // for (emp in obj.employees) {
+      //   console.log(emp);
+      //   str += emp.last_name;
+      //   list.append(`<li>${str}</li>`);
+      //   str = "";
+      // }
+      for (let i = 0; i < 10; i++) {
+        str += `<li>${obj.employees[i].last_name}</li>`;
+        list.innerHTML = str;
+      }
+    } else {
+      console.log("There is error");
+    }
+  };
+  xhr.send();
+}
