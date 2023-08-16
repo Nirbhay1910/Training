db8ad469fa5bb79a5067b64c4ee465215f8855ab
Nirbhay Jain <nirbhay@Nirbhays-MacBook-Air.local>
2023-08-16 17:57:56 +0530

    Done form validation

--- laravel/practice-app/resources/views/register.blade.php
+++ laravel/practice-app/resources/views/register.blade.php
@@ -6,8 +6,29 @@
     <h2>Registeration</h2>
     <form action="{{ url('/') }}/register" method="POST">
         @csrf
-        Email: <input type="email" name="email">
+        {{-- @php
+            print_r($errors->all());
+        @endphp --}}
+        Name: <input type="text" name="name" value={{ old('name') }}>
+        @error('name')
+            {{ $message }}
+        @enderror
+        <br>
+        Email: <input type="email" name="email" value={{ old('email') }}>
+        @error('email')
+            {{ $message }}
+        @enderror
+        <br>
         Password: <input type="password" name="password">
+        @error('password')
+            {{ $message }}
+        @enderror
+        <br>
+        Confirm Password: <input type="password" name="password_confirmation">
+        @error('password_confirmation')
+            {{ $message }}
+        @enderror
+        <br>
         <input type="submit" name="submit" value="submit">
     </form>
 @endsection
